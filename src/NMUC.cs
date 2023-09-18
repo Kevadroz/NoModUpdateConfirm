34b3cd6b46e27ef3683bbc625fa7d0f2f9e2024d
Kevadroz <kevinfdezdominguez@gmail.com>
2023-09-19 01:10:14 +0200

    Initial version

---
+++ src/NMUC.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Security.Permissions;
+using BepInEx;
+using BepInEx.Logging;
+using Menu.Remix.MixedUI;
+using UnityEngine;
+
+#pragma warning disable CS0618
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore CS0618
+
+namespace NMUC;
+
+[BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+
+public class NoModUpdateConfirm : BaseUnityPlugin {
+
+	public const string PLUGIN_GUID = "kevadroz.no_mod_update_confirm";
+	public const string PLUGIN_NAME = "No Mod Update Confirm";
+	public const string PLUGIN_VERSION = "1.0.0";
+
+
+	internal static RemixOptions options;
+	public static new ManualLogSource Logger { get; private set; }
+
+	private void OnEnable() {
+		Logger = base.Logger;
+		options = new RemixOptions(this);
+
+		On.Menu.DialogBoxNotify.ctor += OnNewDialogBox;
+		On.Menu.DialogBoxNotify.Update += OnDialogBoxUpdate;
+
+		On.RainWorld.OnModsInit += OnModsInit;
+	}
+
+	private void OnModsInit( On.RainWorld.orig_OnModsInit orig, RainWorld self ) {
+		MachineConnector.SetRegisteredOI(PLUGIN_GUID, options);
+		orig(self);
+	}
+
+	private bool ShouldAutoConfirm( string signalText ) {
+		return ( options.onModUpdate.Value && signalText == "REAPPLY" )
+		|| ( options.onModReload.Value && signalText == "RESTART" )
+		|| ( options.onGameUpdate.Value && signalText == "VERSIONPROMPT" );
+	}
+
+	private void OnNewDialogBox( On.Menu.DialogBoxNotify.orig_ctor orig, Menu.DialogBoxNotify self, Menu.Menu menu, Menu.MenuObject owner, string text, string signalText, Vector2 pos, Vector2 size, bool forceWrapping ) {
+		orig(self, menu, owner, text, signalText, pos, size, forceWrapping);
+		if ( ShouldAutoConfirm(signalText) )
+			self.RemoveSprites();
+	}
+
+	private void OnDialogBoxUpdate( On.Menu.DialogBoxNotify.orig_Update orig, Menu.DialogBoxNotify self ) {
+		orig(self);
+		if ( ShouldAutoConfirm(self.continueButton.signalText) )
+			self.continueButton.Clicked();
+	}
+
+}
+
+
+internal class RemixOptions : OptionInterface {
+
+	public Configurable<bool> onModUpdate;
+	public Configurable<bool> onModReload;
+	public Configurable<bool> onGameUpdate;
+
+
+	public RemixOptions( NoModUpdateConfirm nmuc ) {
+		onModUpdate = config.Bind("onModUpdate", true);
+		onModReload = config.Bind("onModReload", true);
+		onGameUpdate = config.Bind("onGameUpdate", false);
+	}
+
+	public override void Initialize() {
+
+		OpTab opTab = new(this, "Tab");
+		Tabs = new OpTab[1] { opTab };
+		UIelement[] elements = new UIelement[6]
+		{
+				new OpLabel(40f, 550f, "Skip mod update confirm", bigText: true),
+				new OpCheckBox(onModUpdate, new Vector2(10f, 550f)),
+				new OpLabel(40f, 450f, "Skip mod reload confirm", bigText: true),
+				new OpCheckBox(onModReload, new Vector2(10f, 450f)),
+				new OpLabel(40f, 350f, "Skip game update confirm", bigText: true),
+				new OpCheckBox(onGameUpdate, new Vector2(10f, 350f))
+		};
+		opTab.AddItems(elements);
+
+	}
+
+}
+
